8eb88a3cf447f7bd206d5874b98452b2e29758b9
Ishadi <ishadigamage94@gmail.com>
2018-10-29 09:33:44 +0530

    changes in the controller

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const express = require('express')
+const express = require('express');
 var path = require('path');
 const ControllerRoute = require('./controller/controller');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.use('/controller', ControllerRoute);
-// app.use('/',express.static(__dirname + '/view/index'));
-// app.use('/app',express.static(__dirname + '/view/data'));
+
 
 // viewed at http://localhost:3000
 app.get('/', function(req, res) {
@@ -22,6 +21,5 @@ app.get('/callBack', function (req, res) {
 app.get('/app/data', function (req, res) {
     res.sendFile(__dirname + '/view/data.html');
 });
-//get the token
 
-app.listen(port, () => console.log(`OAuth app listening on port ${port}!`))
+app.listen(port, () => console.log(`OAuth app listening on port ${port}!`));
